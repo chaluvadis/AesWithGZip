2dacc9b28b60a22411a218f1f430cff868e3739f
Surendra Chaluvadi <surendra.chaluvadi@gmail.com>
2023-06-28 16:45:29 +0400

    issue resolved

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using AesDemo;
 using AesDemo.Models;
-DateTime createdOn = DateTime.Now;
-DateTime expiresOn = createdOn.AddDays(30);
+DateTime createdOn = DateTime.Now.AddDays(-31);
+DateTime expiresOn = createdOn.AddDays(-30);
 var originalTokenRequest = new TokenRequest("SHA", "Its a conosle", "2023-1959-789234", "892345678", createdOn, expiresOn);
 
 byte[] key = new byte[32];
@@ -13,19 +13,19 @@ using (var rng = RandomNumberGenerator.Create())
     rng.GetBytes(iv);
 }
 
-Console.WriteLine("--> Original Token Request: " + originalTokenRequest);
+Console.WriteLine($"--> Original Token Request: {originalTokenRequest}");
 byte[] encryptedData = AesHelper.Encrypt<TokenRequest>(originalTokenRequest, key, iv);
-Console.WriteLine("--> Encrypted Data: " + Convert.ToBase64String(encryptedData));
+Console.WriteLine($"{Environment.NewLine}--> Encrypted Data: {Convert.ToBase64String(encryptedData)}");
 var encryptedToken = Convert.ToBase64String(encryptedData);
-Console.WriteLine("--> Encrypted Token " + encryptedToken);
+Console.WriteLine($"{Environment.NewLine}--> Encrypted Token: {encryptedToken}");
 var compressedToken = GzipHelper.Compress(encryptedToken);
-Console.WriteLine("--> Compressed Token " + compressedToken);
+Console.WriteLine($"{Environment.NewLine}--> Compressed Token: {compressedToken}");
 var decompressedToken = GzipHelper.Decompress(compressedToken);
-Console.WriteLine("--> DeCompressed Token " + decompressedToken);
+Console.WriteLine($"{Environment.NewLine}--> DeCompressed Token: {decompressedToken}");
 if (!string.IsNullOrEmpty(decompressedToken))
 {
     var decryptedBytes = Convert.FromBase64String(decompressedToken);
     TokenRequest decryptedTokenRequest = AesHelper.Decrypt<TokenRequest>(decryptedBytes, key, iv);
-    Console.WriteLine("--> Decrypted Token Request: " + decryptedTokenRequest);
-    Console.WriteLine("--> Is Token Valid: " + decryptedTokenRequest.IsTokenValid());
+    Console.WriteLine($"{Environment.NewLine}--> Decrypted Token Request: {decryptedTokenRequest}");
+    Console.WriteLine($"{Environment.NewLine}--> Is Token Valid: {decryptedTokenRequest.IsTokenValid()}");
 }
